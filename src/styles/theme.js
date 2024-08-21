cffa355b2b70058dbb848e29fa827db8c75697b6
dawncoding <sunnuri99@gmail.com>
2024-08-21 18:50:43 +0900

    chore: install Material UI

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#199AE2",
+      dark: "#006BA7",
+    },
+    secondary: {
+      main: "#57C3FF",
+    },
+    background: {
+      default: "#FFFCF0",
+    },
+  },
+});
+
+export default theme;
