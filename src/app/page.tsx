25d54dcf2601800cc33458eb1cc5fe89058d104c
dawncoding <sunnuri99@gmail.com>
2024-08-21 22:17:08 +0900

    style: add custom colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@mui/material";
+import { Button, Chip } from "@mui/material";
 
 export default function Home() {
   return (
@@ -12,6 +12,21 @@ export default function Home() {
       <Button variant="contained" sx={{ backgroundColor: "primary.dark" }}>
         안녕하시와요?
       </Button>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "common.white", color: "common.black" }}
+      >
+        안녕하시와요?
+      </Button>
+      <Chip
+        variant="outlined"
+        label="결근"
+        sx={{
+          backgroundColor: "error.light",
+          borderColor: "error.main",
+          color: "error.main",
+        }}
+      />
     </>
   );
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,6 +12,22 @@ const theme = createTheme({
     background: {
       default: "#FFFCF0",
     },
+    error: {
+      main: "#C62828",
+      light: "#FDC1C1",
+    },
+    warning: {
+      main: "#EF6C00",
+      light: "#FFE8DF",
+    },
+    success: {
+      main: "#FFE8DF",
+      light: "#C4E6C1",
+    },
+    info: {
+      main: "#C4E6C1",
+      light: "#D3F1FF",
+    },
   },
 });
 
