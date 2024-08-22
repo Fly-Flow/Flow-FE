699ce154906e0324cf7e84a19231e9932334c677
Yes2eul <yeseul5611@gmail.com>
2024-08-22 15:22:11 +0900

    feat: add toolbar layout

--- src/components/Employees/index.tsx
+++ src/components/Employees/index.tsx
@@ -1,11 +1,28 @@
-import { Typography } from "@mui/material";
-import SideBar from "../shared/SideBar";
+import { Box, Button, TextField, Typography } from "@mui/material";
+
+const renderToolbar = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "2rem",
+      }}
+    >
+      <TextField size="small" label="이름" />
+      <Button variant="contained">구성원 추가</Button>
+    </Box>
+  );
+};
 
 const Employees: React.FC = (props) => {
   return (
     <>
-      <SideBar />
-      <Typography variant="h4">구성원</Typography>
+      <Typography variant="h4" sx={{ cursor: "default" }}>
+        구성원
+      </Typography>
+      {renderToolbar()}
     </>
   );
 };
