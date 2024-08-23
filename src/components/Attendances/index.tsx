0542d7c407c2c70a560cac5dcca001f38d54ebae
Yes2eul <yeseul5611@gmail.com>
2024-08-23 14:12:30 +0900

    feat: add tabs component

--- src/components/Attendances/index.tsx
+++ src/components/Attendances/index.tsx
@@ -1,7 +1,48 @@
 "use client";
 
-const Attendances: React.FC = (props) => {
-  return <></>;
+import React, { useState } from "react";
+import BasicTabs from "../shared/BasicTabs";
+import { Box, Typography } from "@mui/material";
+
+const Attendances: React.FC = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const tabLabels = [
+    <Typography key="myAttendance" variant="h6" sx={{ cursor: "default" }}>
+      나의 출퇴근
+    </Typography>,
+    <Typography key="allAttendance" variant="h6" sx={{ cursor: "default" }}>
+      전체 출퇴근
+    </Typography>,
+  ];
+
+  const renderMyContents = () => {
+    return <>나의 출퇴근 콘텐츠</>;
+  };
+
+  const renderAllContents = () => {
+    return <>전체 출퇴근 콘텐츠</>;
+  };
+
+  const handleTabChange = (newTab: number) => {
+    setCurrentTab(newTab);
+  };
+
+  return (
+    <Box>
+      <BasicTabs
+        labels={tabLabels}
+        currentTab={currentTab}
+        onTabChange={handleTabChange}
+      />
+      <Box>
+        <Box sx={{ padding: "1rem" }}>
+          {currentTab === 0 && renderMyContents()}
+          {currentTab === 1 && renderAllContents()}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Attendances;
