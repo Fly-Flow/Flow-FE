d774b113afc1b2a5e4e8aa0931b7f9dfbc3b3fe2
dawncoding <sunnuri99@gmail.com>
2024-08-29 18:08:55 +0900

    style: fix theme color code

--- src/components/Leave/index.tsx
+++ src/components/Leave/index.tsx
@@ -1,11 +1,5 @@
-import Header from "../shared/Header";
-
 const Leave: React.FC = (props) => {
-  return (
-    <>
-      <Header header="휴가 관리" />
-    </>
-  );
+  return <></>;
 };
 
 export default Leave;
