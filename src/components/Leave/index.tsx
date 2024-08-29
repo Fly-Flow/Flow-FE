d75b4a7951a6d3ac3603e5e8b16fdb1dc347f659
dawncoding <sunnuri99@gmail.com>
2024-08-29 18:39:24 +0900

    feat(Leave): add BasicTabs component

--- src/components/Leave/index.tsx
+++ src/components/Leave/index.tsx
@@ -1,5 +1,34 @@
+"use client";
+
+import BasicTabs from "@/components/shared/BasicTabs/index.tsx";
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+
 const Leave: React.FC = (props) => {
-  return <></>;
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const tabLabels = [
+    <Typography key={currentTab} variant="h4">
+      나의 휴가 관리
+    </Typography>,
+    <Typography key={currentTab} variant="h4">
+      전체 휴가 관리
+    </Typography>,
+  ];
+
+  const handleTabChange = (newTab: number) => {
+    setCurrentTab(newTab);
+  };
+
+  return (
+    <Box>
+      <BasicTabs
+        labels={tabLabels}
+        currentTab={currentTab}
+        onTabChange={handleTabChange}
+      />
+    </Box>
+  );
 };
 
 export default Leave;
