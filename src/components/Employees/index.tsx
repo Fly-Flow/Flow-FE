c7ff9851104dce938b484be896e4b6db151d38b6
Yes2eul <yeseul5611@gmail.com>
2024-08-29 17:46:14 +0900

    feat: add chip component and edit style colors

--- src/components/Employees/index.tsx
+++ src/components/Employees/index.tsx
@@ -3,6 +3,7 @@
 import {
   Box,
   Button,
+  Chip,
   Dialog,
   DialogActions,
   DialogContent,
@@ -22,6 +23,19 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
+import theme from "@/styles/theme";
+
+type RoleColors = Record<string, string>;
+
+const roleLightColors: RoleColors = {
+  관리자: theme.palette.purple.light,
+  사원: theme.palette.gray.light,
+};
+
+const roleMainColors: RoleColors = {
+  관리자: theme.palette.purple.main,
+  사원: theme.palette.gray.main,
+};
 
 const Employees: React.FC = (props) => {
   const [employeesDialog, setEmployeesDialog] = useState(false);
@@ -119,7 +133,16 @@ const Employees: React.FC = (props) => {
                 <TableCell>{employee.department}</TableCell>
                 <TableCell>{employee.position}</TableCell>
                 <TableCell>{employee.hireDate}</TableCell>
-                <TableCell>{employee.role}</TableCell>
+                <TableCell>
+                  <Chip
+                    label={employee.role}
+                    style={{
+                      backgroundColor: roleLightColors[employee.role],
+                      color: roleMainColors[employee.role],
+                      border: `1px solid ${roleMainColors[employee.role]}`,
+                    }}
+                  />
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -21,12 +21,20 @@ const theme = createTheme({
       light: "#FFE8DF",
     },
     success: {
-      main: "#FFE8DF",
+      main: "#2E7D32",
       light: "#C4E6C1",
     },
     info: {
-      main: "#C4E6C1",
+      main: "#0277BD",
       light: "#D3F1FF",
+    },
+    gray: {
+      main: "#66717E",
+      light: "#D9D9D9",
+    },
+    purple: {
+      main: "#B344F9",
+      light: "#E0B1FD",
     },
   },
 });
