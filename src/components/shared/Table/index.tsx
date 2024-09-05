730a2d7a0efa3bbdcfbc87a4916a4fd49d920a86
dawncoding <sunnuri99@gmail.com>
2024-09-05 20:57:50 +0900

    feat(Table): make onRowClick optional and apply hover/cursor conditionally

--- src/components/shared/Table/index.tsx
+++ src/components/shared/Table/index.tsx
@@ -1,3 +1,4 @@
+import theme from "@/styles/theme.ts";
 import {
   TableContainer,
   Table as MuiTable,
@@ -12,9 +13,10 @@ import React from "react";
 type TableProps = {
   headers: string[];
   rows: (string | number | React.ReactNode)[][];
+  onRowClick?: React.MouseEventHandler<HTMLTableRowElement>;
 };
 
-const Table: React.FC<TableProps> = ({ headers, rows }) => {
+const Table: React.FC<TableProps> = ({ headers, rows, onRowClick }) => {
   return (
     <TableContainer component={Paper}>
       <MuiTable>
@@ -29,7 +31,18 @@ const Table: React.FC<TableProps> = ({ headers, rows }) => {
         </TableHead>
         <TableBody>
           {rows.map((row, rowIndex) => (
-            <TableRow key={rowIndex}>
+            <TableRow
+              key={rowIndex}
+              onClick={onRowClick ? onRowClick : undefined}
+              sx={{
+                cursor: onRowClick ? "pointer" : "default",
+                "&:hover": {
+                  backgroundColor: onRowClick
+                    ? theme.palette.info.light
+                    : undefined,
+                },
+              }}
+            >
               {row.map((cell, cellIndex) => (
                 <TableCell key={cellIndex} align="center">
                   {cell}
